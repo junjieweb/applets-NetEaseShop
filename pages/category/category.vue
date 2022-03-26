3d19ee2d569309d2ce7d81ad8e326053847d59ea
junjieweb <297901657@qq.com>
2022-03-27 01:24:32 +0800

    update

--- pages/category/category.vue
+++ pages/category/category.vue
@@ -1,6 +1,31 @@
 <template>
-  <view>
-    category
+  <view class="categoryContainer">
+    <!-- 头部区域 -->
+    <view class="header">
+      <view class="search">搜索商品</view>
+    </view>
+    
+    <!-- 内容区 -->
+    <view class="contentContainer">
+      <!-- 左侧区域 -->
+      <view class="leftContainer">
+        <scroll-view scroll-y="true" class="navScroll">
+          <view class="navItem">
+            居家生活
+          </view><view class="navItem">
+            居家生活
+          </view><view class="navItem">
+            居家生活
+          </view><view class="navItem">
+            居家生活
+          </view>
+        </scroll-view>
+      </view>
+      
+      <!-- 右侧区域 -->
+      <view class="rightContainer"></view>
+      
+    </view>
   </view>
 </template>
 
@@ -15,5 +40,36 @@
 </script>
 
 <style lang="stylus">
-
+.categoryContainer
+  .header
+    padding 10rpx 0
+    .search
+      width 92%
+      height 60rpx
+      text-align center
+      line-height 60rpx
+      font-size 26rpx
+      background #eee
+      margin 0 auto
+  .contentContainer
+    display flex
+    height calc(100vh - 80rpx)
+    box-sizing border-box
+    border-top 1rpx solid #333
+    .leftContainer
+      width 20%
+      height 100%
+      border-right 1rpx solid #333
+      box-sizing border-box
+      .navScroll
+        height calc(100vh - 82rpx)
+        .navItem
+          font-size 26rpx
+          height 80rpx
+          line-height 80rpx
+          text-align center
+    .rightContainer
+      width 80%
+      height 100%
+      background #d3ffd6
 </style>
