a5a2e1c3d20431cb3ef3c97ec205eb6a922bce80
junjieweb <297901657@qq.com>
2022-03-27 01:29:33 +0800

    add category

--- router/index.js
+++ router/index.js
@@ -1,6 +1,6 @@
-const KoaRouter = require('koa-router')
+const KoaRouter = require("koa-router");
 // 2.生成路由器对象
-const router = new KoaRouter()
+const router = new KoaRouter();
 // express: request response next
 // koa: ctx next
 /*router.get('/test', (ctx) => {
@@ -11,11 +11,17 @@ const router = new KoaRouter()
 })*/
 
 // 注册index主页的接口
-const indexData = require('../datas/index.json')
+const indexData = require("../datas/index.json");
 // console.log(indexData,typeof indexData)
-router.get('/getIndexData', (ctx) => {
-    ctx.body = indexData
-})
+router.get("/getIndexData", (ctx) => {
+  ctx.body = indexData;
+});
+
+// 注册分类页的接口
+const categoryData = require("../datas/categoryDatas.json");
+router.get("/getCategoryData", (ctx) => {
+  ctx.body = categoryData;
+});
 
 // 向外暴露路由器对象
-module.exports = router
+module.exports = router;
